b59449a9d455890a95a5730561b8cddd9acfc183
wangcheng <377414729@qq.com>
2025-09-02 16:13:03 +0800

    博客

---
+++ 任务四：个人博客/awesomeProject/models/book.go
@@ -0,0 +1,68 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "root:woaiwd@tcp(127.0.0.1:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// 配置 GORM 日志器
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // 输出到标准输出，带换行
+		logger.Config{
+			SlowThreshold:             time.Second, // 定义慢 SQL 阈值
+			LogLevel:                  logger.Info, // 关键：设为 Info 级别，显示 SQL
+			IgnoreRecordNotFoundError: true,        // 忽略记录未找到的错误（比如 Find 没查到）
+			Colorful:                  true,        // 启用颜色（可选）
+		},
+	)
+
+	// 打开数据库连接，使用自定义日志器
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger, // 使用我们定义的日志器
+	})
+	if err != nil {
+		log.Fatal("连接数据库失败:", err)
+	}
+	DB = db
+	DB.AutoMigrate(User{}, Post{}, Comment{})
+	var user, user1 User
+	//user.Name = "王五"
+	//DB.Create(&user)
+	DB.First(&user, 1)
+	var post, post1 Post
+	post.ArticleContent = "文章：你好世界"
+	//先把数据都准备好，再进行关联新增
+	stPost := DB.Model(&user).Association("Posts").Append(&post)
+	if stPost != nil {
+		log.Fatal(stPost)
+	}
+	var comment Comment
+	DB.First(&post1, 5)
+	comment.PostID = post1.ID
+	comment.Content = "这文章还真是不错！"
+	DB.Create(&comment)
+	//编写Go代码，使用Gorm查询某个用户发布的所有文章及其对应的评论信息。
+	//先查posts这张表，预加载这张表中存在哪些关联关系，并且自动映射到post结构体中的关联关系字段一次性返回结构体post中
+	//其实正常流程是这句DB.Preload("Posts").Preload("Posts.Comments").First(&user1, 2)
+	//只是存在关联关系可以简写
+	DB.Preload("Posts.Comments").First(&user1, 1)
+	fmt.Println(fmt.Sprintf("用户%s发布的篇文章", user1.Name))
+	for _, post := range user1.Posts {
+		fmt.Printf("文章 ID=%d: %s\n", post.ID, post.ArticleContent)
+		for _, comment := range post.Comments {
+			fmt.Printf("评论 ID=%d: %s\n", comment.ID, comment.Content)
+		}
+	}
+
+}
---
+++ 任务四：个人博客/awesomeProject/models/bookManagement.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// 账户信息表
+type User struct {
+	ID        uint `gorm:"primaryKey"`
+	Name      string
+	PostCount int `gorm:"default:0"` // 文章数量统计
+
+	// ✅ 一对多：一个用户可以有多篇文章
+	Posts []Post `gorm:"foreignKey:UserID"`
+}
+
+func updatePostCommentStatus(tx *gorm.DB, PostID uint) error {
+	var count int64
+	//不管是新增还是删除都先查有没有关联的评论数据，统一返回post要的结果
+	tx.Model(&Comment{}).Where("post_id =?", PostID).Count(&count)
+	state := "有评论"
+	if count == 0 {
+		state = "无评论"
+	}
+	return tx.Model(&Post{}).Where("id=?", PostID).Update("comment_status", state).Error
+}
+
+// 新增钩子
+func (c *Comment) AfterCreate(tx *gorm.DB) error {
+	return updatePostCommentStatus(tx, c.PostID)
+}
+
+// 删除钩子
+func (c *Comment) AfterDelete(tx *gorm.DB) error {
+	return updatePostCommentStatus(tx, c.PostID)
+}
+
+type Post struct {
+	ID             uint `gorm:"primaryKey"`
+	UserID         uint // 外键：指向 User（实现多对一）
+	ArticleContent string
+	CommentStatus  string `gorm:"default:'有评论'"`
+
+	User     User      // 多对一：这篇文章属于哪个用户
+	Comments []Comment `gorm:"foreignKey:PostID"` // ✅ 一对多：一篇文章可以有多个评论
+}
+
+func (p *Post) AfterCreate(tx *gorm.DB) error {
+	return tx.Model(&User{}).Where("id = ?", p.UserID).Update("post_count", gorm.Expr("post_count+1")).Error
+}
+
+type Comment struct {
+	ID      uint `gorm:"primaryKey"`
+	PostID  uint // 外键：指向 Post
+	Content string
+
+	Post Post // 多对一：这条评论属于哪篇文章
+}
